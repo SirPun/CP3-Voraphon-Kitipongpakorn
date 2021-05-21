4fb122cd50b08d887c3f6553657f539b10fe0635
SirPun <84570445+SirPun@users.noreply.github.com>
2021-05-21 22:35:03 +0700

    Exercise4_Voraphon_K.py

---
+++ Exercise4_Voraphon_K.py
@@ -0,0 +1,11 @@
+FD-ENGLISH      = 95.4
+GN_BUSINESS     = 68.97
+INTROCOM-SYSTEM = 45
+COM-PROGRAM     = 78.88
+
+print("----------- Your Score -----------")
+print("Foundation English             "," : ",FD-ENGLISH )
+print("Generall Business              "," : ",GN_BUSINESS )
+print("Introdution to Computer Systems"," : ",INTROCOM-SYSTEM )
+print("Computer Prohgramming          "," : ",COM-PROGRAM  )
+print("----------------------------------")
